68b1d3807f2632f9db8c029de18cfefa46b1d681
gyanaranjan <hi.gyanaranjanpatra@gmail.com>
2024-12-27 14:00:26 +0530

    Feat; Analytics Page

--- app/components/Nabvbar/Navbar.tsx
+++ app/components/Nabvbar/Navbar.tsx
@@ -1,12 +1,11 @@
 import { ModeToggle } from "@/components/toggler";
 import Link from "next/link";
-
 import { SheetDemo } from "@/components/sidebartrigger";
 import Profile from "@/components/Avatar";
 
 export default function Navbar() {
   return (
-    <div className="h-14 w-screen bg-secondary dark:bg-black bg-opacity-50 backdrop-blur-lg flex justify-between px-5 sm:justify-between items-center  ">
+    <div className="h-14 w-screen bg-secondary/30 dark:bg-black/50  backdrop-blur-lg backdrop-saturate-150 shadow-md flex justify-between px-5 sm:justify-between items-center sm:px-20 fixed top-1 z-10 rounded-lg border border-white/10">
       <div className="flex gap-10">
         <div className="sm:hidden">
           <SheetDemo />
@@ -14,27 +13,27 @@ export default function Navbar() {
         <h1 className="text-lg font-bold text-black dark:text-white sm:block hidden">
           IIT KIRBA
         </h1>
-        <div className="hidden gap-4 text-sm text-black dark:text-white items-center sm:flex ">
+        <div className="hidden gap-4 text-sm text-black dark:text-white items-center sm:flex">
           <Link
-            className="dark:text-gray-300 hover:underline underline-offset-2 "
+            className="dark:text-gray-300 hover:underline underline-offset-2"
             href="/"
           >
             Analytics
           </Link>
           <Link
-            className="dark:text-gray-300 hover:underline underline-offset-2 "
+            className="dark:text-gray-300 hover:underline underline-offset-2"
             href="/"
           >
             Users
           </Link>
           <Link
-            className="dark:text-gray-300 hover:underline underline-offset-2 "
+            className="dark:text-gray-300 hover:underline underline-offset-2"
             href="/"
           >
             Branch
           </Link>
           <Link
-            className="dark:text-gray-300 hover:underline underline-offset-2 "
+            className="dark:text-gray-300 hover:underline underline-offset-2"
             href="/"
           >
             Materials
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,26 @@
-import VisitorChart from '@/components/visitorchart'
-import React from 'react'
-import Component from '@/components/visitorsgraph'
+import VisitorChart from "@/components/visitorchart";
+import React from "react";
+import Component from "@/components/visitorsgraph";
+import BranchWiseVisitors from "@/components/branchwise";
+import { MonthwiseVisitors } from "@/components/monthwisevisitor";
+import { TotalVisitor } from "@/components/totalvisitor";
 
 
 export default function page() {
   return (
-    <div className=''>
-      <h1 className='text-4xl pt-2 pl-2 mb-6'>Analytics</h1>
-      <div className='w-[100vw] flex flex-col justify-center items-center'>
-          <VisitorChart/>
-          <Component/>
+    <div className="min-h-screen w-screen bg-secondary dark:bg-zinc-950  mt-10  flex justify-center">
+      <div className="w-[90vw] min-h-screen bg-secondary dark:bg-zinc-950">
+        <h1 className="text-4xl pt-10">Analytics</h1>
+        <div className="w-[90vw] min-h-screen  pt-2 flex flex-col gap-2">
+          <div className="flex  max-sm:flex-col hover:cursor-pointer justify-between  ">
+            <VisitorChart />
+            <BranchWiseVisitors />
+            <MonthwiseVisitors/>
+            <TotalVisitor/>
+          </div>
+          <Component />
+        </div>
       </div>
     </div>
-  )
+  );
 }
